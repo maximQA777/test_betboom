d33aedaf9e0ecd4c294986f45485c2c7ada68e90
Maksim Strgogv <alabai.vfrcbv2001@gmail.com>
2025-03-19 11:51:56 +0300

    Домашка по проекту

--- test_ui/conftest.py
+++ test_ui/conftest.py
@@ -1,17 +1,32 @@
 import pytest
 from selene.support.shared import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
+from dotenv import load_dotenv
+import os
+
+from test_ui import attach
+
+DEFAULT_BROWSER_VERSION = "100.0"
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
 
 
-
-
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
-    browser.config.base_url = 'https://betboom.ru'
-    browser.open('/')
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options  # чтоб тест выполнялся когда сайт продолжается грузиться , но html загрузился
@@ -20,7 +35,32 @@ def setup_browser(request):
     # driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер , надо для этого 8 , 10 строчку кода
     # driver_options.add_argument('--headless')
 
+    SELENOID_LOGIN = os.getenv("SELENOID_LOGIN")
+    SELENOID_PASS = os.getenv("SELENOID_PASS")
+    SELENOID_URL = os.getenv("SELENOID_URL")
 
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "125.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    browser.config.driver = webdriver.Remote(
+        command_executor=f"https://{SELENOID_LOGIN}:{SELENOID_PASS}@{SELENOID_URL}/wd/hub",
+        options=options)
+
+
+    yield
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+    browser.quit()
 
 
 
